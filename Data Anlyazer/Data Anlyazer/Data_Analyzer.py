c9cf52db22f70a2bc6f2643e31699c79d68faded
Clinton Dmello <clinton.dmello95@outlook.com>
2020-04-06 21:36:42 +0530

    Commiting Initial version of Lines Graph

--- Data Anlyazer/Data Anlyazer/Data_Analyzer.py
+++ Data Anlyazer/Data Anlyazer/Data_Analyzer.py
@@ -1,8 +1,33 @@
 import matplotlib.pyplot as plt
+from matplotlib import interactive
 import numpy as np
 import pandas as pd
 
-gasdata = pd.readcsv('gas_prices.csv')
-plt.plot(gas.Year, gas.USA)
+gasdata = pd.read_csv('./Data/gas_prices.csv')
+selectiveData = plt.figure(0) 
+plt.figure(figsize=(8,5))
+plt.xticks(gasdata.Year[::2])
+plt.xlabel('Year')
+plt.ylabel('US Dollars')
+plt.title('Gas Prices Range (in USD)')
+plt.plot(gasdata['Year'], gasdata['USA'],'r.',label='United States of America')
+plt.plot(gasdata['Year'],gasdata['Mexico'],'g.-', label='Mexico')
+plt.plot(gasdata['Year'],gasdata['South Korea'],'y.--', label='South Korea')
+plt.plot(gasdata['Year'],gasdata['Germany'],'b.-', label='Germany')
+plt.legend()
+
+automappedData  = plt.figure(1)
+plt.figure(figsize=(8,5))
+plt.xticks(gasdata.Year[::2])
+plt.xlabel('Year')
+plt.ylabel('US Dollars')
+plt.title('Gas Prices Range (in USD)')
+for country in gasdata:
+    if country != 'Year':
+        plt.plot(gasdata.Year,gasdata[country],label=country)
+plt.legend()
 plt.show()
+
+
+
 
